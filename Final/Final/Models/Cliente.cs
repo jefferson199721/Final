b62ad7a7d42f46a503ecd7fd505c65d78c8c222e
jefferson199721 <jeffersonmvinueza@gmail.com>
2019-01-30 14:14:07 -0500

    clientes

---
+++ Final/Final/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Categoria { get; set; }
+        public string Garantia { get; set; }
+        public DateTime FechaIngreso { get; set; }
+    }
+}
