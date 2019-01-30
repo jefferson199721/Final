0ef15a33209c0746adaa2eca5d3d97254e557f67
jefferson199721 <jeffersonmvinueza@gmail.com>
2019-01-30 15:40:57 -0500

    Vistas

--- Final/Final/Data/Migrations/20190130203852_clientes.Designer.cs
+++ Final/Final/Data/Migrations/20190130203852_clientes.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Final.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190130203852_clientes")]
+    partial class clientes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
