472f2d94363f25668b2b40fcb7ff76d2bae96e31
jefferson199721 <jeffersonmvinueza@gmail.com>
2019-01-30 14:27:55 -0500

    Bien

--- Final/Final/Models/Cliente.cs
+++ Final/Final/Models/Cliente.cs
@@ -8,11 +8,11 @@ namespace Final.Models
     public class Cliente
     {
         public int ClienteId { get; set; }
-        public string NombresJEff123ggggggg { get; set; }
+        public string Nombres { get; set; }
         public string Apellidos { get; set; }
         public string Telefono { get; set; }
         public string Correo { get; set; }
-        public string CategoriaSSSSSSSSSSSSSSSSS { get; set; }
+        public string Categoria { get; set; }
         public string Garantia { get; set; }
         public DateTime FechaIngreso { get; set; }
     }
