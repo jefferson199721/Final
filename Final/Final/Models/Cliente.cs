f105158c7d2db5b30a88d411aeafcd13c6c901a7
ericko_1709 <macara.123@hotmail.com>
2019-01-30 14:40:15 -0500

    erico

--- Final/Final/Models/Cliente.cs
+++ Final/Final/Models/Cliente.cs
@@ -11,7 +11,7 @@ namespace Final.Models
         public string Nombres { get; set; }
         public string Apellidos { get; set; }
         public string Telefono { get; set; }
-        public string Correo { get; set; }
+        public string Correossssss { get; set; }
         public string Categoria{ get; set; }
         public string Garantia { get; set; }
         public DateTime FechaIngreso { get; set; }
